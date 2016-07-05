eb24f4f25c24993eb5aadde3879e9a25b420db8c
Shaked Edri <ShakedEdri@Shakeds-MacBook-Pro.local>
2016-07-05 17:05:26 -0400

    first commit

---
+++ extra.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#about").on("click", function( e ) {
+
+        e.preventDefault();
+
+        $("body, html").animate({ 
+            scrollTop: $( $(this).attr('href') ).offset().top 
+        }, 600);
+
+    });
+});
+
+
+$(document).ready(function() {
+    $("#contact").on("click", function( e ) {
+
+        e.preventDefault();
+
+        $("body, html").animate({ 
+            scrollTop: $( $(this).attr('href') ).offset().top 
+        }, 600);
+
+    });
+});
+
+
